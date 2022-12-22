fa0cb7288efdaa14353a2b1cf896e513fac2aaec
Mailine Nguyen <64129348+MailineN@users.noreply.github.com>
2022-12-22 15:17:39 +0100

    feat: add back office server uri

--- src/main/java/com/protools/flowableDemo/configuration/SwaggerConfiguration.java
+++ src/main/java/com/protools/flowableDemo/configuration/SwaggerConfiguration.java
@@ -1,10 +1,12 @@
 package com.protools.flowableDemo.configuration;
 
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,14 +17,20 @@ public class SwaggerConfiguration {
     @Autowired
     BuildProperties buildProperties;
 
+    @Value("${fr.insee.protools.server.uri}")
+    String serverUri;
+
     Contact contact = new Contact()
         .name("Protools")
             .url("https://github.com/InseeFr/Protools-Back-Office");
+
+
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .components(new Components())
+                .addServersItem(new Server().url(serverUri))
                 .info(new Info()
                         .title(buildProperties.getName())
                         .description("Back-Office Service for Protools orchestrator")
