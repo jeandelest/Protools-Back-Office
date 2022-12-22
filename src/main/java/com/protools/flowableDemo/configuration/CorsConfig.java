e7ee4374ebbea6f6f5edf713ab7e2e0837371193
Mailine Nguyen <64129348+MailineN@users.noreply.github.com>
2022-12-22 14:42:30 +0100

    feat: add WebSecurityConfig

---
+++ src/main/java/com/protools/flowableDemo/configuration/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.protools.flowableDemo.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    /**
+     * Temporary authorize all origins
+     */
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setMaxAge(3600L);
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+}
--- src/main/java/com/protools/flowableDemo/configuration/SwaggerConfiguration.java
+++ src/main/java/com/protools/flowableDemo/configuration/SwaggerConfiguration.java
@@ -2,6 +2,7 @@ package com.protools.flowableDemo.configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
@@ -14,14 +15,20 @@ public class SwaggerConfiguration {
     @Autowired
     BuildProperties buildProperties;
 
+    Contact contact = new Contact()
+        .name("Protools")
+            .url("https://github.com/InseeFr/Protools-Back-Office");
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .components(new Components())
                 .info(new Info()
                         .title(buildProperties.getName())
-                        .description("POC Protools avec Flowable BPMN Engine")
-                        .version(buildProperties.getVersion()));
+                        .description("Back-Office Service for Protools orchestrator")
+                        .version(buildProperties.getVersion())
+                        .contact(contact));
+
     }
 
 
