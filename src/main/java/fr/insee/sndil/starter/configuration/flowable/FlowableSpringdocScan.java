e1588cae95d526f992c8a395366e2df7b448b745
Marc Berger <marc.berger@insee.fr>
2023-02-28 14:43:05 +0100

    essais pour afficher l'api rest dans le swagger

--- src/main/java/fr/insee/sndil/starter/configuration/flowable/FlowableSpringdocScan.java
+++ src/main/java/fr/insee/sndil/starter/configuration/flowable/FlowableSpringdocScan.java
@@ -1,4 +1,4 @@
-package fr.insee.sndil.starter.tmp;
+package fr.insee.sndil.starter.configuration.flowable;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.flowable.rest.service.api.RestResponseFactory;
@@ -7,15 +7,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-
+/**
+ * This class allows springdoc to expose the flowable REST API :
+ */
 @Configuration
+//The component scan so the beans (@RestController) will be availables
 @ComponentScan(basePackages = "org.flowable.rest.service.api")
-
-public class SwaggerDocumentationConfig {
+public class FlowableSpringdocScan {
 
     @Autowired
     protected ObjectMapper objectMapper;
-    
+
+    //We need to define this bean which is required by a bean initialized by the @ComponentScan
     @Bean()
     public RestResponseFactory restResponseFactory() {
         RestResponseFactory restResponseFactory = new RestResponseFactory(objectMapper);
