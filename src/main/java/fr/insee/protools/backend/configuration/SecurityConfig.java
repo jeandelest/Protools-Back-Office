49a34d6b8a9ad824a4714489fb184d4557fc6bbd
Marc Berger <marc.berger@insee.fr>
2023-09-22 14:45:15 +0200

    fix pour springboot 3.1.4

--- src/main/java/fr/insee/protools/backend/configuration/SecurityConfig.java
+++ src/main/java/fr/insee/protools/backend/configuration/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -27,91 +28,106 @@ import java.util.stream.Collectors;
 @Slf4j
 public class SecurityConfig {
 
-        public static final String STARTER_SECURITY_ENABLED = "fr.insee.sndil.starter.security.enabled";
+    public static final String STARTER_SECURITY_ENABLED = "fr.insee.sndil.starter.security.enabled";
 
 
-        // Démonstration avec un rôle protégeant l'accès à un des endpoints
-        @Value("${fr.insee.sndil.starter.role.administrateur}")
-        private String administrateurRole;
+    // Démonstration avec un rôle protégeant l'accès à un des endpoints
+    @Value("${fr.insee.sndil.starter.role.administrateur}")
+    private String administrateurRole;
 
-        //Par défaut, spring sécurity prefixe les rôles avec cette chaine
-        private static final String ROLE_PREFIX = "ROLE_";
+    //Par défaut, spring sécurity prefixe les rôles avec cette chaine
+    private static final String ROLE_PREFIX = "ROLE_";
 
-        @Autowired InseeSecurityTokenProperties inseeSecurityTokenProperties;
+    @Autowired InseeSecurityTokenProperties inseeSecurityTokenProperties;
 
-        //Liste d'URL sur lesquels on n'applique pas de sécurité (swagger; actuator...)
-        @Value("#{'${fr.insee.sndil.starter.security.whitelist-matchers}'.split(',')}")
-        private String[] whiteList;
+    //Liste d'URL sur lesquels on n'applique pas de sécurité (swagger; actuator...)
+    @Value("#{'${fr.insee.sndil.starter.security.whitelist-matchers}'.split(',')}")
+    private String[] whiteList;
 
-        //Filter with activated security
-        @Bean
-        @ConditionalOnProperty(name = STARTER_SECURITY_ENABLED,  havingValue = "true")
-        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-                http.csrf(AbstractHttpConfigurer::disable)
-                    .authorizeHttpRequests(authorize ->
-                        authorize.requestMatchers(whiteList).permitAll()
-                              //  .requestMatchers("/**").permitAll()
-                            .requestMatchers("/starter/healthcheck").permitAll()
-                            .requestMatchers("/starter/healthcheckadmin").hasRole(administrateurRole)
-                            //We allow admin to access everything
-                            .requestMatchers("/**").hasRole(administrateurRole)
-                    )
-                    .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
-                    .oauth2ResourceServer(oauth2 ->oauth2.jwt(jwt ->jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
-                return http.build();
-        }
-        //Filter with disabled security
-        @Bean
-        @ConditionalOnProperty(name = STARTER_SECURITY_ENABLED,  havingValue = "false", matchIfMissing = true)
-        public SecurityFilterChain filterChain_noSecurity(HttpSecurity http) throws Exception {
+    //Filter with activated security
+    @Bean
+    @ConditionalOnProperty(name = STARTER_SECURITY_ENABLED, havingValue = "true")
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
 
-                http.csrf(AbstractHttpConfigurer::disable)
-                        .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
-                        .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
-                        .oauth2ResourceServer(oauth2 ->oauth2.jwt(jwt ->jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
-                return http.build();
+        for (var pattern : whiteList) {
+            http.authorizeHttpRequests(authorize ->
+                    authorize
+                            .requestMatchers(AntPathRequestMatcher.antMatcher(pattern)).permitAll()
+            );
         }
 
-        @Bean
-        JwtAuthenticationConverter jwtAuthenticationConverter() {
-                JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-                jwtAuthenticationConverter.setPrincipalClaimName(inseeSecurityTokenProperties.getOidcClaimUsername());
-                jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter());
-                return jwtAuthenticationConverter;
-        }
+        http.authorizeHttpRequests(authorize ->
+                        authorize
+                                //  .requestMatchers("/**").permitAll()
+                                //  .requestMatchers(mvcMatcherBuilder.pattern("/api/users/**")).permitAll() //MVC Controller
+                                //  .requestMatchers(AntPathRequestMatcher.antMatcher(h2ConsolePath + "/**")).permitAll()
+                                //  .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.GET,"/cars")).permitAll() //REST
+                                .requestMatchers(AntPathRequestMatcher.antMatcher("/starter/healthcheck")).permitAll() //REST
+                                .requestMatchers(AntPathRequestMatcher.antMatcher("/starter/healthcheckadmin")).hasRole(administrateurRole)
+                                //We allow admin to access everything
+                                .requestMatchers(AntPathRequestMatcher.antMatcher("/**")).hasRole(administrateurRole)
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
+        return http.build();
+    }
 
-        Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter() {
-                return new Converter<Jwt, Collection<GrantedAuthority>>() {
-                        @Override @SuppressWarnings({ "unchecked" }) public Collection<GrantedAuthority> convert(Jwt source) {
+    //Filter with disabled security
+    @Bean
+    @ConditionalOnProperty(name = STARTER_SECURITY_ENABLED, havingValue = "false", matchIfMissing = true)
+    public SecurityFilterChain filterChain_noSecurity(HttpSecurity http) throws Exception {
 
-                                String[] claimPath = inseeSecurityTokenProperties.getOidcClaimRole().split("\\.");
-                                Map<String, Object> claims = source.getClaims();
-                                try {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
+        return http.build();
+    }
 
-                                        for (int i = 0; i < claimPath.length - 1; i++) {
-                                                claims = (Map<String, Object>) claims.get(claimPath[i]);
-                                        }
+    @Bean
+    JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setPrincipalClaimName(inseeSecurityTokenProperties.getOidcClaimUsername());
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter());
+        return jwtAuthenticationConverter;
+    }
 
-                                        List<String> roles = (List<String>) claims.getOrDefault(claimPath[claimPath.length - 1], new ArrayList<>());
-                                        //if we need to add customs roles to every connected user we could define this variable (static or from properties)
-                                        //roles.addAll(defaultRolesForUsers);
-                                        return roles.stream().map(s -> new GrantedAuthority() {
-                                                @Override public String getAuthority() {
-                                                        return ROLE_PREFIX + s;
-                                                }
+    Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter() {
+        return new Converter<Jwt, Collection<GrantedAuthority>>() {
+            @Override
+            @SuppressWarnings({"unchecked"})
+            public Collection<GrantedAuthority> convert(Jwt source) {
 
-                                                @Override public String toString() {
-                                                        return getAuthority();
-                                                }
-                                        }).collect(Collectors.toUnmodifiableList());
-                                }
-                                catch (ClassCastException e) {
-                                        // role path not correctly found, assume that no role for this user
-                                        return new ArrayList<>();
-                                }
+                String[] claimPath = inseeSecurityTokenProperties.getOidcClaimRole().split("\\.");
+                Map<String, Object> claims = source.getClaims();
+                try {
+
+                    for (int i = 0; i < claimPath.length - 1; i++) {
+                        claims = (Map<String, Object>) claims.get(claimPath[i]);
+                    }
+
+                    List<String> roles = (List<String>) claims.getOrDefault(claimPath[claimPath.length - 1], new ArrayList<>());
+                    //if we need to add customs roles to every connected user we could define this variable (static or from properties)
+                    //roles.addAll(defaultRolesForUsers);
+                    return roles.stream().map(s -> new GrantedAuthority() {
+                        @Override
+                        public String getAuthority() {
+                            return ROLE_PREFIX + s;
                         }
-                };
-        }
+
+                        @Override
+                        public String toString() {
+                            return getAuthority();
+                        }
+                    }).collect(Collectors.toUnmodifiableList());
+                } catch (ClassCastException e) {
+                    // role path not correctly found, assume that no role for this user
+                    return new ArrayList<>();
+                }
+            }
+        };
+    }
 }
 
 
