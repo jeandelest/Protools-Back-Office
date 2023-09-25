a924419052a36fd4ea27e6bcb1ff588042317a7c
Marc Berger <marc.berger@insee.fr>
2023-09-25 14:16:54 +0200

    Ajout d'une classe ProtoolsBpmnError qui a vocation à être parent de nos classes métier ; Hack : elle inverse message et code car dans les sous process de traitement de ces évènements, on n'a accès qu'au code et que donc ce n'est pas pratique pour logger

--- src/main/java/fr/insee/protools/backend/exception/ProtoolsBpmnError.java
+++ src/main/java/fr/insee/protools/backend/exception/ProtoolsBpmnError.java
@@ -7,7 +7,10 @@ public class ProtoolsBpmnError extends BpmnError {
         super(errorCode);
     }
 
+    //Hack : currently we do not use the error code and use the message as both the errorcode and the message
+    //Because we cannot get the message from within an eventSubprocess used to handle the bpmn error
+    //OF course it is should be reverted when we find the solution
     public ProtoolsBpmnError(String errorCode, String message) {
-        super(message,errorCode);
+        super(message,message);
     }
 }
