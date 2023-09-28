47d983971b654f750b31854c2190139111171ae2
Marc Berger <marc.berger@insee.fr>
2023-09-28 16:03:14 +0200

    remove an useless import

--- src/test/java/fr/insee/protools/backend/webclient/WebClientHelperTest.java
+++ src/test/java/fr/insee/protools/backend/webclient/WebClientHelperTest.java
@@ -19,7 +19,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.core.NestedRuntimeException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
